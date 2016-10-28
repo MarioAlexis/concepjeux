0aa517a8af7fc4b4338aa29f21f75ce84ef42834
Mario Alexis <mario.alexis88@gmail.com>
2016-10-28 00:30:14 -0400

    officialement done la spec n04

--- TP02/Jeu de course/Assets/BananaTrigger.cs
+++ TP02/Jeu de course/Assets/BananaTrigger.cs
@@ -1,49 +1,108 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 using System.Collections;
 
 public class BananaTrigger : MonoBehaviour
 {
-    float spintime = 3.0f;
-    float currentSpinTime = 0.0f;
-    float distance = 20.0f;
-    bool isSpin = false;
-    float currentangle;
-    Transform carTrans;
-    Rigidbody carRigi;
-    Vector3 velo;
+    // SERIALIZE CUSTOM EDITOR
+    public float SlowDownRatio = 0.5f;
+    public float RespawnDelaySec = 2.0f;
+    public bool RespawnAfterTrigger = false;
+
+    // PRIVATE GLOBAL VARIABLES
+    private bool isSpin = false;
+    private bool isAfterSpin = false;
+    private float spinTour = 0;
+    private float accDeg = 0;
+    private float angleRot = 7f;
+    private float currentangle;
+    private float accDelay = 0.0f;
+    private Transform carTrans;
+    private Rigidbody carRigi;
+    private Vector3 velo;
+    private Vector3 bananaInitPos;
     void OnTriggerEnter(Collider car)
     {
-        Debug.Log("collider : " +car);
-        Debug.Log(car.transform.parent.transform.parent.name);
-        carTrans = car.transform.parent.transform.parent.gameObject.GetComponent<Transform>();
-        carRigi = car.transform.parent.transform.parent.gameObject.GetComponent<Rigidbody>();
-        isSpin = true;
-        //this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 2, this.transform.position.z);
-        velo = carRigi.velocity;
-        Debug.Log(velo);
-        Update();
+        if(!isSpin)
+        {
+            Debug.Log(" " + SlowDownRatio + " " + RespawnDelaySec + " " + RespawnAfterTrigger);
+            carTrans = car.transform.parent.transform.parent.gameObject.GetComponent<Transform>();
+            carRigi = car.transform.parent.transform.parent.gameObject.GetComponent<Rigidbody>();
+            velo = carRigi.velocity;
+            if (velo.magnitude >= 0 && velo.magnitude <= 15)
+                spinTour = 1;
+            else if (velo.magnitude >= 16 && velo.magnitude <= 30)
+                spinTour = 2;
+            else if (velo.magnitude >= 31 && velo.magnitude <= 45)
+                spinTour = 3;
+            carRigi.velocity = Vector3.zero;
+            if(RespawnAfterTrigger)
+            {
+                this.transform.position = new Vector3(this.transform.position.x, -3.0f, this.transform.position.z);
+                isAfterSpin = true;
+            }
+            isSpin = true;
+        }
+    }
+
+    void Start()
+    {
+        bananaInitPos = this.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        if (RespawnAfterTrigger && isAfterSpin)
+        {
+            if (accDelay < RespawnDelaySec)
+            {
+                accDelay += Time.deltaTime;
+            }
+            else
+            {
+                this.transform.position = bananaInitPos;
+                accDelay = 0;
+                isAfterSpin = false;
+            }
+        }
     }
 
     void Update()
     {
-        if(isSpin && currentSpinTime <= spintime)
+        if(isSpin && (accDeg < (360 * spinTour)))
         {
-            //Debug.Log("spin state");
-            currentSpinTime += Time.deltaTime;
-            //currentangle = carTrans.rotation.eulerAngles.y;
-            //Debug.Log("angle : " + currentangle);
-            Debug.Log("VELO ==> " + carRigi.velocity);
-            Debug.Log("ANGULAR ==> " + carRigi.angularVelocity);
-           // carTrans.Rotate(Vector3.up, Time.deltaTime * 250);
-            carTrans.Translate(Time.deltaTime * 15, 0.0f, 0.0f,Space.World);
-           // carRigi.angularVelocity = carRigi.angularVelocity * 0.05f;
-            //carTrans.Rotate(0, currentangle+Time.deltaTime, 0, Space.World);
-           // carTrans.Rotate(Vector3.up, Time.deltaTime*250);
+            Vector3 currentPos = carTrans.position;
+            Vector3 newVelo = (velo * SlowDownRatio) * Time.deltaTime;
+            Vector3 newPos = currentPos + newVelo; ;
 
+            currentangle = carTrans.rotation.eulerAngles.y;
+            carTrans.eulerAngles = new Vector3(0.0f, currentangle + angleRot, 0.0f);
+            accDeg = accDeg + angleRot;
+            carTrans.position = newPos;
         }
-        else
+        else if (isSpin && (accDeg >= (360 * spinTour)))
         {
+            // a repenser pour donner une petite acceleration
+            carRigi.velocity = velo * SlowDownRatio * 0.5f;
             isSpin = false;
+            accDeg = 0;
+        }
+    }
+}
+
+[CustomEditor(typeof(BananaTrigger))]
+public class MyEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        BananaTrigger mybanana = (BananaTrigger)target;
+
+        mybanana.SlowDownRatio = EditorGUILayout.Slider("Slow Down Ratio", mybanana.SlowDownRatio, 0.0f, 1.0f);
+        mybanana.RespawnAfterTrigger = EditorGUILayout.Toggle("Respawn After Trigger ?", mybanana.RespawnAfterTrigger);
+
+        if (mybanana.RespawnAfterTrigger)
+        {
+            mybanana.RespawnDelaySec = EditorGUILayout.Slider("Respawn Delay Sec", mybanana.RespawnDelaySec, 1.0f, 10.0f);
         }
     }
 }
