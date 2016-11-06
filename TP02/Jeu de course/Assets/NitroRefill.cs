1163a18be005809c8ca489846dcf64c093c32c0e
Erwan <marchand.erwan@gmail.com>
2016-11-06 15:55:25 -0500

    objet collec NitroBoost

---
+++ TP02/Jeu de course/Assets/NitroRefill.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Vehicles.Car;
+
+public class NitroRefill : MonoBehaviour {
+
+    public CarController carController;
+
+    public float RespawnDelaySec = 5.0f;
+
+    private Vector3 boostInitPos;
+
+    float timer = 0f;
+
+    bool needToReset = false;
+
+    void OnTriggerEnter(Collider car)
+    {
+        if (car.gameObject.tag == "Joueur 1")
+        {
+            carController.addNitro(25);
+            this.transform.position = new Vector3(this.transform.position.x, -3.0f, this.transform.position.z);
+            timer = 0f;
+            needToReset = true;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        boostInitPos = this.transform.position;
+    }
+
+   
+
+    // Update is called once per frame
+    void Update () {
+	    timer += (Time.deltaTime);
+        if (needToReset && timer > RespawnDelaySec)
+        {
+            this.transform.position = boostInitPos;
+            needToReset = false;
+        }
+    }
+}
