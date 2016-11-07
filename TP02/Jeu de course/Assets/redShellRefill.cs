a73124fa727068963060716790ac7a41be93de4e
Erwan <marchand.erwan@gmail.com>
2016-11-06 22:09:09 -0500

    ajout refill shells

---
+++ TP02/Jeu de course/Assets/redShellRefill.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class redShellRefill : MonoBehaviour {
+
+    public shellSpawn shell;
+
+    public float RespawnDelaySec = 5.0f;
+
+    private Vector3 refillInitPos;
+
+    float timer = 0f;
+
+    bool needToReset = false;
+
+    void OnTriggerEnter(Collider car)
+    {
+        if (car.gameObject.tag == "Joueur 1")
+        {
+            shell.refillRedShell();
+            this.transform.position = new Vector3(this.transform.position.x, -3.0f, this.transform.position.z);
+            timer = 0f;
+            needToReset = true;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        refillInitPos = this.transform.position;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += (Time.deltaTime);
+        if (needToReset && timer > RespawnDelaySec)
+        {
+            this.transform.position = refillInitPos;
+            needToReset = false;
+        }
+        transform.Rotate(0, Time.deltaTime * 80, 0, Space.World);
+    }
+}
