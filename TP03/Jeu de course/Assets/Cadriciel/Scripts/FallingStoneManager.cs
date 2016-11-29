8af672bbb5981db31d2eb439d79fc10b313fceee
Mario Alexis <mario.alexis88@gmail.com>
2016-11-29 01:20:33 -0500

    Start new mecha gameplay

---
+++ TP03/Jeu de course/Assets/Cadriciel/Scripts/FallingStoneManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingStoneManager : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] spawnPosList;
+    [SerializeField]
+    GameObject Stone;
+    [SerializeField]
+    float RespawnTime = 2.0f;
+    [SerializeField]
+    float ForceToBottom = 10.0f;
+
+    // PRIVATES VARIABLES
+    private float timeAcc = 0.0f;
+    private int actualPos = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+    }
+	
+    void FixedUpdate()
+    {
+        timeAcc += Time.deltaTime;
+        if(timeAcc >= RespawnTime)
+        {
+            GameObject stoneinscene = Instantiate(Stone, spawnPosList[actualPos].position, Quaternion.identity) as GameObject;
+            stoneinscene.GetComponent<Rigidbody>().AddRelativeForce((Vector3.down * ForceToBottom), ForceMode.Acceleration);
+            actualPos = (actualPos + 1) % 3;
+            timeAcc = 0.0f;
+        }
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	
+	}
+}
