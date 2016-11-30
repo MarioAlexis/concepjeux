ff3983b5b20f67fe97e815670870b2a2bd642b03
Erwan <marchand.erwan@gmail.com>
2016-11-30 16:05:10 -0500

    ajout triggers virages

---
+++ TP03/Jeu de course/Assets/showVirageArrowDroite.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class showVirageArrowDroite : MonoBehaviour {
+
+    public RightArrowSignalScript arrow;
+    public Collider playerOneBodyCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other == playerOneBodyCollider)
+        {
+            arrow.activateArrow();
+        }
+        //Debug.Log("Entering Zone");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other == playerOneBodyCollider)
+        {
+            arrow.desactivateArrow();
+        }
+        //Debug.Log("Leaving Zone");
+    }
+}
