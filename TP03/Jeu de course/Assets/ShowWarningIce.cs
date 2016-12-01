e60342ca27642f97f2d8944bc44e37275692f752
Erwan <marchand.erwan@gmail.com>
2016-12-01 16:44:04 -0500

    warning sign

---
+++ TP03/Jeu de course/Assets/ShowWarningIce.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowWarningIce : MonoBehaviour {
+
+    public WarningIceSignalScript arrow;
+    public Collider playerOneBodyCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other == playerOneBodyCollider)
+        {
+            arrow.activateArrow();
+        }
+        //Debug.Log("Entering Zone");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other == playerOneBodyCollider)
+        {
+            arrow.desactivateArrow();
+        }
+        //Debug.Log("Leaving Zone");
+    }
+}
---
+++ TP03/Jeu de course/Assets/WarningIceSignalScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WarningIceSignalScript : MonoBehaviour {
+
+    public Scrollbar warningSign;
+    float timer = 0;
+    bool showArrow;
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = 0;
+        showArrow = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += (Time.deltaTime) ;
+        if (showArrow)
+        {
+            if (timer % 1f < 0.5f)
+            {
+                warningSign.size = 1f;
+            }
+            else
+                warningSign.size = 0f;
+        }
+            
+        else
+            warningSign.size = 0f;
+    }
+
+    public void activateArrow()
+    {
+        showArrow = true;
+        timer = 0;
+    }
+
+    public void desactivateArrow()
+    {
+        showArrow = false;
+    }
+}
