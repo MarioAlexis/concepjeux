e7c8b6ca814fc37ea1a0c5aa2b2df8f83bbf752d
Mario Alexis <mario.alexis88@gmail.com>
2016-11-30 23:13:14 -0500

    icy prefab added

---
+++ TP03/Jeu de course/Assets/Cadriciel/Scripts/SlideEffect.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlideEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+   void OnTriggerEnter(Collider car)
+    {
+        if(car.tag == "player")
+        {
+            Debug.Log("Player glisse");
+            Transform wheels = car.gameObject.transform.parent.transform.parent.Find("WheelsHubs");
+            Debug.Log(wheels.name);
+            int nbChild = wheels.childCount;
+            Debug.Log(nbChild);
+            for (int i=0; i < nbChild; i++)
+            {
+                Transform child = wheels.GetChild(i);
+                Debug.Log(child.name);
+                WheelCollider singleWheel = child.GetComponent<WheelCollider>();
+                WheelFrictionCurve frictionCurve = singleWheel.sidewaysFriction;
+                frictionCurve.stiffness = 0.1f;
+                singleWheel.sidewaysFriction = frictionCurve;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider car)
+    {
+        if (car.tag == "player")
+        {
+            Debug.Log("Player glisse");
+            Transform wheels = car.gameObject.transform.parent.transform.parent.Find("WheelsHubs");
+            Debug.Log(wheels.name);
+            int nbChild = wheels.childCount;
+            Debug.Log(nbChild);
+            for (int i = 0; i < nbChild; i++)
+            {
+                Transform child = wheels.GetChild(i);
+                Debug.Log(child.name);
+                WheelCollider singleWheel = child.GetComponent<WheelCollider>();
+                WheelFrictionCurve frictionCurve = singleWheel.sidewaysFriction;
+                frictionCurve.stiffness = 1.0f;
+                singleWheel.sidewaysFriction = frictionCurve;
+            }
+        }
+    }
+}
